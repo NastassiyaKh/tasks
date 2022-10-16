6e1e5968bd44137250cf3cc1fe5e2941610f9c89
NastassiyaKh <nholodok@yandex.ru>
2022-10-16 12:57:34 +0300

    task 2_2 second

--- src/Box.java
+++ src/Box.java
@@ -3,27 +3,26 @@ public class Box {
     private final int width;
     private final int height;
 
-    public Box(int lenght, int width, int height){
+    public Box(int lenght, int width, int height) {
         this.lenght = lenght;
         this.width = width;
         this.height = height;
     }
 
-    public static String getBoxType(int lenght, int width, int height){
+    public String getBoxType() {
         String type = " ";
-        if (lenght == width && lenght == height){
+        if (this.lenght == this.width && this.lenght == this.height) {
             type = "Коробка является кубом";
-        }
-        else if (height != 0){
+        } else if (this.height != 0) {
             type = "Коробка является обычной коробкой";
+        } else {
+            type = "Коробка является конвертом";
         }
-        else if (height == 0) {
-            type = "Коробка является ковертом";
-        };
         return type;
     }
 
     public static void main(String[] args) {
-        System.out.println(getBoxType (1, 1, 0));
+        Box box = new Box (1,1,0);
+        System.out.println(box.getBoxType());
     }
 }
